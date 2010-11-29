e6110706b197285e9ca16dd5bf37fbc8b9c022fc
Jon Frisby <jfrisby@mrjoy.com>
2010-11-28 18:18:53 -0800

    Add toolbar helpers.

---
+++ Editor/EditorGUIToolbar.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEditor;
+
+// Extensions in the spirit/style of GUILayout.*
+public static class EditorGUILayoutToolbar {
+  public const int BUTTON_SPACE = 6;
+  public static void Space() {
+    GUILayout.Space(BUTTON_SPACE);
+  }
+
+  public static void FlexibleSpace() {
+    GUILayout.Label(GUIHelper.NoContent, GUIHelper.NoStyle, GUIHelper.ExpandWidth);
+  }
+
+  public static void Begin() {
+    GUILayout.BeginHorizontal(EditorStyles.toolbar, GUIHelper.ExpandWidth);
+  }
+
+  public static void End() {
+    GUILayout.Label(GUIContent.none, GUIStyle.none, GUIHelper.ExpandWidth);
+    GUILayout.EndHorizontal();
+  }
+}
